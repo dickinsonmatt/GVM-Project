a322a0e45d32255db230214665731bb21b5677dc
Matthew Dickinson <matthew.dickinson.1019@gmail.com>
2023-10-17 13:31:10 -0400

    API script for survey data

---
+++ api_call_script.R
@@ -0,0 +1,10 @@
+library(qualtRics)
+
+qualtrics_api_credentials(api_key = "",
+                          base_url = "",
+                          install = TRUE,
+                          overwrite = TRUE)
+
+
+
+surveys <- all_surveys() 
